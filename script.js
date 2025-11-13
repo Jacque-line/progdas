4ec33afcab11a8714444a28a9da2ade276a9e416
Jacque-line <blsmk.250127050@budiluhur.sch.idm>
2025-11-13 15:11:12 +0700

    add scrolltotop

--- script.js
+++ script.js
@@ -19,3 +19,16 @@ if (form){
         form.reset();
     })
 }
+
+const scrollTopBtn = document.getElementById("scrollTopBtn");
+window.onscroll = () => {
+    if(document.documentElement.scrollTop > 200) {
+        scrollTopBtn.style.display = "block";
+    } else {
+        scrollTopBtn.style.display = "none";
+    }
+}
+
+scrollTopBtn.addEventListener("click", () => {
+    window.screenTop({top: 0, behavior: "smooth"});
+})
